796c634079cdfe5ae07b55029c1716c773266e5b
RyanCohen <rrcohen@stanford.edu>
2019-05-06 12:07:03 -0700

    Tuned acceleration boundaries to meetdesired constraints.

--- speed_profile.m
+++ speed_profile.m
@@ -2,6 +2,8 @@ clc
 clear
 close all
 a_max = 4; a_xmax = 3; a_xmin = -4; a_ymax = 4;
+a_max = 0.99*a_max;
+a_xmin = 0.985*a_xmin;
 load('project_data.mat')
 
 max_front = islocalmax(path.k_1pm, 'FlatSelection', 'first'); % Start of regions of maximum curvature
@@ -19,7 +21,7 @@ for i = 1:length(max_front)
     if in_max == 0
         if max_front(i) == 1
             start = i;
-            vel_profiles(sum(max_front(1:start)), 1:start) = integrate_backwards(path.s_m(1:start), path.k_1pm(1:start), a_max);
+            vel_profiles(sum(max_front(1:start)), 1:start) = integrate_backwards(path.s_m(1:start), path.k_1pm(1:start), a_max, a_xmin);
             in_max = 1;
         end
     else
@@ -31,7 +33,7 @@ for i = 1:length(max_front)
     end
 end
 
-vel_profiles(end - 1, :) = const_accel(path.s_m, a_xmax);
+vel_profiles(end - 1, :) = const_accel(path.s_m, 0.85*a_xmax);
 vel_profiles(end , :) = const_accel(path.s_m, a_xmin);
 
 figure
@@ -45,8 +47,8 @@ title('Final (Minimum) Desired Velocity Profile'); xlabel('s [m]'); ylabel('Ux [
 
 final_ax = zeros(1, length(final_vel));
 final_ay = zeros(1, length(final_vel));
-for i = 1:(length(final_vel) - 1)
-    final_ax(i) = (final_vel(i+1) - final_vel(i))/(path.s_m(i+1) - path.s_m(i)) * final_vel(i);
+for i = 2:(length(final_vel) - 1)
+    final_ax(i) = (final_vel(i+1) - final_vel(i))/(path.s_m(i+1) - path.s_m(i)) * final_vel(i+1);
     final_ay(i) = path.k_1pm(i) * final_vel(i)^2;
 end
 final_a = sqrt(final_ax.^2 + final_ay.^2);
@@ -56,8 +58,12 @@ plot(path.s_m, final_ax, path.s_m, final_ay, path.s_m, final_a)
 legend('a_x', 'a_y', 'a')
 title('Desired Acceleration Profiles'); xlabel('s [m]'); ylabel('acceleration [m/s^2]');
 
+max(real(final_ax(3:end)))
+min(real(final_ax))
+max(real(final_ay))
+max(real(final_a(3:end)))
 
-function Ux = integrate_backwards(s, K, a_max)
+function Ux = integrate_backwards(s, K, a_max, a_xmin)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -65,6 +71,9 @@ function Ux = integrate_backwards(s, K, a_max)
     Ux(end) = sqrt(a_max/K(end));
     for i = (length(Ux) - 1):-1:1
         dUxds = sqrt(a_max^2 - (K(i+1)*Ux(i+1)^2)^2)/Ux(i+1);
+%         if dUxds*Ux(i+1) > abs(a_xmin) % Enforce longitudinal acceleration constraint
+%             dUxds = a_xmin/Ux(i+1);
+%         end
         Ux(i) = Ux(i + 1) + dUxds*(s(i+1) - s(i));
     end
 end
